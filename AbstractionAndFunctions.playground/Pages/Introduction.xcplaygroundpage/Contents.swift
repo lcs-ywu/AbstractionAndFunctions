36a459fdf83e9af61111bf4671cf7cb840fdc67d
Russell Gordon <github@russellgordon.ca>
2020-02-10 10:07:52 -0500

    Initial Commit

---
+++ AbstractionAndFunctions.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ # Introduction
+  
+ - Callout(Example):
+  What does this message say?
+ 
+    `084 111 100 097 121 032 105 115 032 084 104 117 114 115 100 097 121 044 032 070 101 098 114 117 097 114 121 032 054 044 032 050 048 050 048 046`
+
+ Not sure about that? Unable to read [ASCII codes](http://www.asciitable.com)? That would be entirely normal!
+ 
+ OK, how about this instead?
+ 
+ - Callout(Example):
+  What does this message say?
+ 
+    `01010100 01101111 01100100 01100001 01111001 00100000 01101001 01110011 00100000 01010100 01101000 01110101 01110010 01110011 01100100 01100001 01111001 00101100 00100000 01000110 01100101 01100010 01110010 01110101 01100001 01110010 01111001 00100000 00110110 00101100 00100000 00110010 00110000 00110010 00110000 00101110 `
+
+
+ Most humans cannot directly read binary, either!
+  
+ Page 1 | [Next: About Abstractions](@next)
+ 
+ */
+
+
