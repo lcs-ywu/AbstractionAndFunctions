216c7b864e38b2ed2908f54a3a373b974f7b2cc6
James Wu <ywu@lcs.on.ca>
2020-02-20 16:39:29 -0500

    Finished all functions

--- AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
+++ AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
@@ -39,6 +39,44 @@ func trapezoidParameter(a:Double,b:Double,c:Double,d:Double) -> Double {
 }
 print("The parameter of the trapezoid is \(trapezoidParameter(a:1,b:2,c:3,d:4))" )
 
+//Area for circles
+func circleArea(radius:Double) -> Double {
+    return Double.pi * pow(radius,2)
+}
+print("The area of the circle is " + String(format:"%.2f",circleArea(radius:5.0)) )
+
+//Area for trapezoids
+func trapezoidArea(a:Double,b:Double,h:Double) -> Double {
+    return h * (a+b) / 2
+}
+print("The area of the trapezoid is \(trapezoidArea(a:1,b:3,h:5))" )
+
+//Surface area of a cylinder
+func cylinderSurfaceArea(r:Double,h:Double) -> Double {
+    return 2 * Double.pi * ((pow(r, 2))+r*h)
+}
+print("The surface area of the cylinder is " +  String(format:"%.2f",cylinderSurfaceArea(r: 5, h: 5)))
+
+//Surface area of a Squarebased pyramid
+func areaOfSquarebasedPyramid(b:Double,s:Double) -> Double {
+    return pow(b, 2) + 2 * b * s
+}
+print("The surface area of the cylinder is \(areaOfSquarebasedPyramid(b: 5, s: 5))")
+
+//Volume of a sphere
+func VolumeOfSphere(r:Double) -> Double {
+    return (4*Double.pi*pow(r, 3))/3
+}
+print("The volume of the cylinder is " + String(format:"%.2f",VolumeOfSphere(r: 5)))
+
+//Volume of a cone
+func VolumeOfCones(r:Double,h:Double) -> Double {
+    return Double.pi*pow(r,2)*h/3
+}
+print("The volume of the cone is " + String(format:"%.2f",VolumeOfCones(r: 5,h:5)))
+
+
+
 
 
 
